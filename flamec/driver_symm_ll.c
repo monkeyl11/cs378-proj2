2ca8ade00da0ad642232b9d317b8837413c66558
monkeyl11 <lu.andrew@gmail.com>
2024-03-27 23:37:14 -0500

    ewfiodhu

--- flamec/driver_symm_ll.c
+++ flamec/driver_symm_ll.c
@@ -14,6 +14,7 @@ void symm_ll_unb_var1( FLA_Obj, FLA_Obj, FLA_Obj );
 int main(int argc, char *argv[])
 {
   int n, nfirst, nlast, ninc, i, irep, nrepeats;
+  int m = 123;
 
   double
     dtime, dtime_best, 
@@ -21,9 +22,9 @@ int main(int argc, char *argv[])
 
   dtime_best = 0.0;
 
-  FLA_Obj y, x, A; 
+  FLA_Obj A, B, C; 
 
-  FLA_Obj yref, yold;
+  FLA_Obj Cref, Cold;
   
   /* Initialize FLAME. */
   FLA_Init( );
@@ -44,27 +45,26 @@ int main(int argc, char *argv[])
   for ( n=nfirst; n<= nlast; n+=ninc ){
 
     /* Allocate space for the matrices and vectors */
-    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, 1, &y );
-    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, 1, &yref );
-    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, 1, &yold );
+    FLA_Obj_create( FLA_DOUBLE, n, m, 1, n, &C );
+    FLA_Obj_create( FLA_DOUBLE, n, m, 1, n, &Cref );
+    FLA_Obj_create( FLA_DOUBLE, n, m, 1, n, &Cold );
     FLA_Obj_create( FLA_DOUBLE, n, n, 1, n, &A );
-    FLA_Obj_create( FLA_DOUBLE, n, 1, 1, 1, &x );
+    FLA_Obj_create( FLA_DOUBLE, n, m, 1, n, &B );
 
     /* Generate random matrix A, and vectors x, and y */
-    FLA_Random_matrix( A );
-    FLA_Random_matrix( yold );
-    FLA_Random_matrix( x );
-
-
+    FLA_Random_symm_matrix( FLA_LOWER_TRIANGULAR, A );
+    FLA_Random_matrix( Cold );
+    FLA_Random_matrix( B );
 
     for ( irep=0; irep<nrepeats; irep++ ) {
     /* Time reference implementation (from libflame) */
-      FLA_Copy( yold, yref );
+      FLA_Copy( Cold, Cref );
     
       /* start clock */
       dtime = FLA_Clock();
     
-      FLA_Gemv( FLA_NO_TRANSPOSE, FLA_ONE, A, x, FLA_ONE, yref );
+      //FLA_Gemv( FLA_NO_TRANSPOSE, FLA_ONE, A, x, FLA_ONE, yref );
+      FLA_Gemm(FLA_NO_TRANSPOSE, FLA_NO_TRANSPOSE, FLA_ONE, A, B, FLA_ONE, Cref);
  
       /* stop clock */
       dtime = FLA_Clock() - dtime;
@@ -77,18 +77,19 @@ int main(int argc, char *argv[])
 
     printf( "data_ref( %d, 1:2 ) = [ %d %le ];\n", i, n, dtime_best );
     fflush( stdout );
+    FLA_Triangularize( FLA_LOWER_TRIANGULAR, FLA_NONUNIT_DIAG, A );
 
     /* Time your unblocked Variant 1 */
 
     for ( irep=0; irep<nrepeats; irep++ ){
       /* Copy vector yold to y */
-      FLA_Copy( yold, y );
+      FLA_Copy( Cold, C );
     
       /* start clock */
       dtime = FLA_Clock();
  
       /* Comment out the below call and call your routine instead */
-      //gemv_unb_var1( y, A, x );
+      symm_ll_unb_var1( A, B, C );
       /* stop clock */
       dtime = FLA_Clock() - dtime;
     
@@ -98,18 +99,24 @@ int main(int argc, char *argv[])
 	dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
     }
 
-    diff = FLA_Max_elemwise_diff( y, yref );
+    diff = FLA_Max_elemwise_diff( C, Cref );
+    // FLA_Obj_show( char* header, FLA_Obj obj, char* format, char* footer );
+    // FLA_Obj_show( "A", A, "%f", "A" );
+    // FLA_Obj_show( "B", B, "%f", "B" );
+    // FLA_Obj_show( "Cold", Cold, "%f", "Cold" );
+    // FLA_Obj_show( "C", C, "%f", "C" );
+    // FLA_Obj_show( "Cref", Cref, "%f", "Cref" );
   
     printf( "data_unb_var1( %d, 1:3 ) = [ %d %le %le];\n", i, n,
 	    dtime_best, diff  );
 
     fflush( stdout );
 
-    FLA_Obj_free( &y );
-    FLA_Obj_free( &yold );
-    FLA_Obj_free( &yref );
+    FLA_Obj_free( &C );
+    FLA_Obj_free( &Cold );
+    FLA_Obj_free( &Cref );
     FLA_Obj_free( &A );
-    FLA_Obj_free( &x );
+    FLA_Obj_free( &B );
 
     i++;
   }
